92f330079b907107e452f6aa359a4d7d483865c4
Jonathan Dursi <jonathan@dursi.ca>
2019-02-16 12:24:34 -0500

    Working with dredd tests

--- python/step1/main.py
+++ python/step1/main.py
@@ -5,33 +5,29 @@ Front end of Individual/Variant/Call API example
 """
 import datetime
 import logging
-import sys
 import connexion
 import yaml
 from connexion import NoContent
 from bravado_core.spec import Spec
 
 #
-# Read in the API definition, and parse it with Bravado
+# Bravado core can create Python classes from objects in the Swagger spec
 #
 SPEC_DICT = yaml.safe_load(open('swagger.yaml', 'r'))
 SWAGGER_SPEC = Spec.from_dict(SPEC_DICT, config={'use_models': True})
 
-#
-# Generate the Python API models from the spec
-#
 Individual = SWAGGER_SPEC.definitions['Individual']  # pylint:disable=invalid-name
 Variant = SWAGGER_SPEC.definitions['Variant']  # pylint:disable=invalid-name
 Call = SWAGGER_SPEC.definitions['Call']  # pylint:disable=invalid-name
 
 #
-# examples
+# Example Individual, Variant, and Calls:
 #
 test_individual = Individual(id=1, description='Patient Zero',
                              created=datetime.datetime.now())
 
 test_variant = Variant(id=1, chromosome='chr1', start=14370,
-                       ref='G', alt='A', description='rs6054257',
+                       ref='G', alt='A', name='rs6054257',
                        created=datetime.datetime.now())
 
 test_call = Call(id=1, variant_id=1, individual_id=1,
@@ -42,6 +38,7 @@ def get_variants(chromosome, start, end):
     """
     Return all variants between [chrom, start) and (chrom, end]
     """
+    logging.info("inside get_variants")
     return [test_variant], 200
 
 
@@ -49,7 +46,7 @@ def get_individuals():
     """
     Return all individuals
     """
-    print("get_individuals")
+    logging.info("inside get_individuals")
     return [test_individual], 200
 
 
@@ -58,6 +55,7 @@ def get_calls():
     Return all calls
     """
     print("get_calls")
+    logging.info("inside get_calls")
     return [test_call], 200
 
 
@@ -65,7 +63,7 @@ def post_variant(variant):
     """
     Add a new variant
     """
-    print('Got variant:', variant, file=sys.stderr)
+    logging.info("Got variant: %s", str(variant))
     return NoContent, 201
 
 
@@ -73,7 +71,7 @@ def post_individual(individual):
     """
     Add a new individual
     """
-    print('Got individual:', individual, file=sys.stderr)
+    logging.info("Got individual: %s", str(individual))
     return NoContent, 201
 
 
@@ -81,14 +79,14 @@ def post_call(call):
     """
     Add a new call
     """
-    print('Got call:', call, file=sys.stderr)
+    logging.info("Got call: %s", str(call))
     return NoContent, 201
 
 
 logging.basicConfig(level=logging.INFO)
 
 app = connexion.FlaskApp(__name__)
-app.add_api('swagger.yaml')
+app.add_api('swagger.yaml', strict_validation=False)
 
 application = app.app
 
@@ -101,4 +99,4 @@ def shutdown_session(exception=None):
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=5000)
